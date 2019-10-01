c495f21972e7c3279efc77fe117780590eb809cd
gabrielgs44 <gabriel.gomes.silva.999@gmail.com>
2019-10-01 20:30:23 -0300

    Trabalho pronto

---
+++ TesteCliente/Program.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace TesteCliente
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            EchoCliente.Echo echo = new EchoCliente.EchoClient();
+
+            Console.Write("Digite sua Frase: ");
+
+            var body = new EchoCliente.echoRequestBody(Console.ReadLine());
+            var request = new EchoCliente.echoRequest(body);
+            var response = echo.echo(request);
+
+            Console.WriteLine(response.Body.@return);
+            Console.ReadLine();
+
+        }
+    }
+}
